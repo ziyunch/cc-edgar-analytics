24fffb5e901d72b8cd2ebfc6291683f0c4d6911a
ziyunch <ziyunch@gmail.com>
2019-02-14 10:22:04 -0500

    worked version 1.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# Coding challenge
+
+## Requirements
+Python 3.6.5
+
+## Problem statement
+### Problem description
+Write a program to read in time-ordered weblog file and count requests made in each session.
+
+### Input format
+`log.csv`: time-ordered line-separated lists containing comma-delimited `ip`(string), `date` and `time`
+>**ip,date,time**,zone,cik,accession,extention,code,size,idx,norefer,noagent,find,crawler,browser
+`inactivity_period.txt`: single integer range from 1 to 86,400.
+
+### Output format
+`sessionization.txt`: line-separated lists containing comma-delimited `ip`, first date&time in session, last date&time in session, duration, count of requests.
+
+## Approach
+1. Prepare
+   - list of dict to store ip in its current active session log `active_log=[past_active_log{ip:event_index}]`
+   - dict of active ip and information `ip_dict{ip:session_start_date, session_start_time, session_start_second, session_last_active_date, session_last_active_time, session_last_active_second, event_count, first_event_index}`
+
+2. For each line, read in `ip`, `date` and `time`, update ip information in `ip_dict`, add ip in current session log (use `temp`), remove ip in last active session if possible.
+
+3. Session log store in a reusable manner. Output all ip information in session log exceed the `act_period` in the order of `first_event_index`, assign current session log to it. Remove all ip in `ip_dict`.
+
+## Comment
+1. The time complexity is O(nlogn).
+2. Maybe use list of sets to save memory.
---
+++ src/sessionization.py
@@ -0,0 +1,123 @@
+import sys
+import datetime
+
+def read_line(line):
+    """ Process each line
+    """
+    csv_row = line.split(",")
+    ip = csv_row[0]
+    date = csv_row[1]
+    time = csv_row[2]
+    datetime_str = date + " " + time
+    format_str = "%Y-%m-%d %H:%M:%S"
+    datetime_obj = datetime.datetime.strptime(datetime_str, format_str)
+    return ip, datetime_obj, date, time
+
+def time_diff(dt, dt0):
+    """ Calculate difference between two datetime object and return in seconds
+    """
+    diff_obj = dt - dt0
+    diff = int(diff_obj.total_seconds())
+    return diff
+
+def clean_active_log(num):
+    """ evoke inactive sessions log and write to file
+    """
+    global active_log
+    global ip_dict
+    global f2
+    sorted(active_log[num].values())
+    for ip, noth in active_log[num].items():
+        output_session(ip)
+
+def output_session(ip):
+    """ Pop inactive ip and output in file
+    """
+    global ip_dict
+    session_start = ip_dict[ip][0] + " " + ip_dict[ip][1]
+    session_end = ip_dict[ip][3] + " " + ip_dict[ip][4]
+    duration = str(time_diff(ip_dict[ip][5], ip_dict[ip][2])+1)
+    count = str(ip_dict[ip][6])
+    output_list = []
+    output_list = [ip, session_start, session_end, duration, count]
+    output = ",".join(output_list)
+    ip_dict.pop(ip)
+    f2.write(output+"\n")
+
+def process_weblog(weblog_file, output_file, inact_period):
+    """ Process Weblog
+    """
+    global active_log
+    global ip_dict
+    global f2
+    # Initialize the dictionaries
+    ip_dict = {}
+    ip_dict[0] = []
+    temp = dict()
+    active_log = [dict() for _ in range(0, inact_period)]
+    # Read in the weblog file
+    with open(weblog_file) as f1, open(output_file, "a") as f2:
+        f1.readline()
+        # Read in first ip event and initialize first ip session
+        line = f1.readline()
+        ip, dt, date, time = read_line(line)
+        dt0 = dt
+        time_count = 0
+        event_index = 1
+        mod_value = time_count % inact_period
+        ip_dict[ip] = [date, time, dt, date, time, dt, 1, event_index]
+        temp[ip] = event_index
+        for line in f1:
+            # Read in next ip event
+            ip, dt, date, time = read_line(line)
+            event_index += 1
+            diff = time_diff(dt, dt0)
+            if diff > time_count:
+                # For every second, evoke inactive sessions and write to file
+                clean_active_log(mod_value)
+                active_log[mod_value] = temp
+                if ip in ip_dict:
+                    # pop ip from last active session log
+                    last_time_count = time_diff(ip_dict[ip][5], dt0)
+                    last_mod = last_time_count % inact_period
+                    active_log[last_mod].pop(ip)
+                time_count = diff
+                mod_value = time_count % inact_period
+                temp = dict()
+            # Update ip dictionary for active sessions
+            # Add ip in updated current active session log with event index
+            if ip not in ip_dict:
+                # Initialize ip in this session
+                ip_dict[ip] = [date, time, dt, date, time, dt, 1, event_index]
+                temp[ip] = event_index
+            else:
+                # Update current session's last update time and event count
+                ip_dict[ip][3] = date
+                ip_dict[ip][4] = time
+                ip_dict[ip][5] = dt
+                ip_dict[ip][6] += 1
+                temp[ip] = ip_dict[ip][7]
+        # Deal with the remaining ip sessions
+        remaindt = dict()
+        remaindt.update(active_log[mod_value])
+        active_log[mod_value] = temp
+        for i in range(0, inact_period):
+            remaindt.update(active_log[i])
+        for ip in sorted(remaindt, key=remaindt.get):
+            output_session(ip)
+
+def main():
+    if len(sys.argv) != 4:
+        print("Four arguments are expected: [window.txt], [actual.txt], [predicted.txt], [comparison.txt]")
+        exit(1)
+    # Input
+    weblog_file = sys.argv[1]
+    inact_period_file = sys.argv[2]
+    output_file = sys.argv[3]
+    # Read window value from `inactivity_period_file`, O(1)
+    inact_period = int(open(inact_period_file).read())
+    # Process weblog
+    process_weblog(weblog_file, output_file, inact_period)
+
+if __name__ == "__main__":
+    main()
